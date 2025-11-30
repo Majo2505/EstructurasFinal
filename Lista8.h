031fc2fdf0e0d3f70a00f6b4c357d5e2551cb692
ArianaPita-0102 <ariana.pita@ucb.edu.bo>
2025-11-30 16:54:48 -0400

    Clase Lista8 limpia

--- Lista8.h
+++ Lista8.h
@@ -2,7 +2,6 @@
 #include "Tripla.h"
 
 template<class T>
-
 class Lista8
 {
 private:
@@ -11,20 +10,169 @@ private:
 public:
 	Lista8();
 	~Lista8();
-	Tripla<T>* getPrimerTripla();
-	Tripla<T>* getUltimaTripla();
-	bool insertar_Principio(T el, int peso);
+
+	bool insertar_Final(T el);
+	void mostrar();
+
+	// --- MÉTODOS CLAVE PARA KARGER ---
+
+	// 1. Fusión O(1): Pega una lista al final de otra sin recorrerla
+	void absorberLista(Lista8<T>& otraLista);
+
+	// 2. Requisito Obligatorio: Elimina TODAS las apariciones de un nodo
+	// (Necesario para eliminar físicamente los autociclos de la lista)
+	void eliminar_todos(T el);
+};
+
+template<class T>
+inline Lista8<T>::Lista8()
+{
+	primer = NULL;
+	ultimo = NULL;
+}
+
+// Destructor Iterativo (Seguro y Rápido)
+template<class T>
+inline Lista8<T>::~Lista8()
+{
+	Tripla<T>* aux = primer;
+	while (aux != NULL)
+	{
+		Tripla<T>* siguiente = aux->getSiguiente();
+		delete aux;
+		aux = siguiente;
+	}
+	primer = NULL;
+	ultimo = NULL;
+}
+
+template<class T>
+inline bool Lista8<T>::insertar_Final(T el)
+{
+	if (ultimo == NULL)
+	{
+		ultimo = new Tripla<T>(NULL, el, NULL);
+		primer = ultimo;
+	}
+	else
+	{
+		Tripla<T>* nuevo = new Tripla<T>(ultimo, el, NULL);
+		ultimo->setTriplaSig(nuevo);
+		ultimo = nuevo;
+	}
+	return true;
+}
+
+template<class T>
+inline void Lista8<T>::mostrar()
+{
+	Tripla<T>* aux = primer;
+	while (aux != NULL)
+	{
+		cout << " " << aux->getElemento();
+		aux = aux->getSiguiente();
+	}
+	cout << endl;
+}
+
+// --- FUSIÓN RÁPIDA (O(1)) ---
+template<class T>
+inline void Lista8<T>::absorberLista(Lista8<T>& otraLista)
+{
+	if (otraLista.primer == NULL) return; // Nada que absorber
+
+	if (this->primer == NULL)
+	{
+		// Si yo estoy vacío, tomo la otra lista entera
+		this->primer = otraLista.primer;
+		this->ultimo = otraLista.ultimo;
+	}
+	else
+	{
+		// Conecto mi final con el principio de la otra
+		this->ultimo->setTriplaSig(otraLista.primer);
+		otraLista.primer->setTriplaAnt(this->ultimo);
+
+		// Mi nuevo último es el último de la otra
+		this->ultimo = otraLista.ultimo;
+	}
+
+	// Vaciamos la otra lista para evitar errores de memoria al destruirla
+	otraLista.primer = NULL;
+	otraLista.ultimo = NULL;
+}
+
+// --- ELIMINACIÓN TOTAL (Para borrar autociclos) ---
+template<class T>
+inline void Lista8<T>::eliminar_todos(T el)
+{
+	Tripla<T>* actual = primer;
+
+	while (actual != NULL)
+	{
+		// Guardamos el siguiente antes de borrar, para no perder el hilo
+		Tripla<T>* siguiente = actual->getSiguiente();
+
+		if (actual->getElemento() == el)
+		{
+			// Lógica de desvinculación
+
+			// 1. Es el primero
+			if (actual == primer)
+			{
+				primer = primer->getSiguiente();
+				if (primer != NULL) primer->setTriplaAnt(NULL);
+				else ultimo = NULL; // Quedó vacía
+			}
+			// 2. Es el último
+			else if (actual == ultimo)
+			{
+				ultimo = ultimo->getAnterior();
+				ultimo->setTriplaSig(NULL);
+			}
+			// 3. Está en medio
+			else
+			{
+				actual->getAnterior()->setTriplaSig(actual->getSiguiente());
+				actual->getSiguiente()->setTriplaAnt(actual->getAnterior());
+			}
+
+			// Borramos nodo y prevenimos errores
+			actual->setTriplaSig(NULL);
+			delete actual;
+		}
+
+		// Avanzamos
+		actual = siguiente;
+	}
+}
+
+/*
+//version 2
+#pragma once
+#include "Tripla.h"
+
+template<class T>
+class Lista8
+{
+private:
+	Tripla<T>* primer;
+	Tripla<T>* ultimo;
+public:
+	Lista8();
+	~Lista8();
 	bool insertar_Final(T el);
 	bool eliminar_Principio();
 	bool eliminar_Final();
-	bool buscarR(T el, Tripla<T>* t);
-	bool buscar(T el);
-	Tripla<T>* buscarTripla(T el);
 	void mostrar();
-	void mostrarconPesosFinal();
 	bool eliminar_el_dado(T el);
-	void llenar_Lista8con1M(T n);
-	Tripla<T>* buscarEnesimo(int pos);
+	inline Tripla<T>* getPrimerTripla();
+
+	// NUEVA FUNCIÓN CLAVE PARA KARGER (Velocidad O(1))
+	// Pasa todos los elementos de 'otraLista' al final de esta lista.
+	
+	void absorberLista(Lista8<T>& otraLista);
+	inline void eliminar_todos(T el);
 };
 
 template<class T>
@@ -37,39 +185,21 @@ inline Lista8<T>::Lista8()
 template<class T>
 inline Lista8<T>::~Lista8()
 {
-	if (primer != NULL)
+	Tripla<T>* aux = primer;
+	while (aux != NULL)
 	{
-		delete primer; //hara una eliminacion recursiva de todos las triplas q contenga, por el destructor de la tripla
+		Tripla<T>* siguiente = aux->getSiguiente();
+		delete aux;
+		aux = siguiente;
 	}
+	primer = NULL;
+	ultimo = NULL;
 }
 
 template<class T>
 inline Tripla<T>* Lista8<T>::getPrimerTripla()
 {
 	return primer;
-}
-
-template<class T>
-inline Tripla<T>* Lista8<T>::getUltimaTripla()
-{
-	return ultimo;
-}
-
-template<class T>
-inline bool Lista8<T>::insertar_Principio(T el, int peso)
-{
-	bool resp = true;
-	if (primer == NULL)
-	{
-		primer = new Tripla<T>(NULL, el, NULL, peso);
-		ultimo = primer;
-	}
-	else
-	{
-		primer = new Tripla<T>(NULL, el, primer, peso);
-		primer->getSiguiente()->setTriplaAnt(primer);
-	}
-	return resp;
 }
 
 template<class T>
@@ -83,8 +213,10 @@ inline bool Lista8<T>::insertar_Final(T el)
 	}
 	else
 	{
-		ultimo = new Tripla<T>(ultimo, el, NULL);
-		ultimo->getAnterior()->setTriplaSig(ultimo);
+		// Creamos la nueva tripla apuntando atras al actual ultimo
+		Tripla<T>* nuevo = new Tripla<T>(ultimo, el, NULL);
+		ultimo->setTriplaSig(nuevo);
+		ultimo = nuevo;
 	}
 	return resp;
 }
@@ -99,20 +231,18 @@ inline bool Lista8<T>::eliminar_Principio()
 	}
 	else
 	{
+		Tripla<T>* aux = primer; // Guardamos referencia para borrar luego
 		if (primer == ultimo)  // solo un elemento en la lista
 		{
 			primer = NULL;
 			ultimo = NULL;
-			delete primer;
 		}
 		else
 		{
-			Tripla<T>* aux = primer;
 			primer = primer->getSiguiente();
-			primer->setTriplaAnt(NULL); //y ponemos q su anterior sea null para q deje de apuntar al antiguo primero
-			aux->setTriplaSig(NULL);//ponemos q su sig del aux sea null para no tener problemas con el destructor de la tripla
-			delete aux;
+			primer->setTriplaAnt(NULL);
 		}
+		delete aux; // Borramos la memoria
 	}
 	return resp;
 }
@@ -127,79 +257,22 @@ inline bool Lista8<T>::eliminar_Final()
 	}
 	else
 	{
+		Tripla<T>* aux = ultimo; // Guardamos referencia
 		if (primer == ultimo)  //  un elemento en la lista
 		{
 			primer = NULL;
 			ultimo = NULL;
-			delete ultimo;
 		}
 		else
 		{
-			Tripla<T>* aux = ultimo;
-			ultimo = ultimo->getAnterior(); //movemos el ultimo al antepenultimo
-			ultimo->setTriplaSig(NULL); // y ponemos q su sig sea null para q deje de apuntar al antiguo ultimo
-			aux->setTriplaSig(NULL);//ponemos q su sig sea null para no tener problemas con el destructor de la tripla
-			delete aux;
+			ultimo = ultimo->getAnterior();
+			ultimo->setTriplaSig(NULL);
 		}
-	}
-
-	return resp;
-}
-
-template<class T>
-inline bool Lista8<T>::buscarR(T el, Tripla<T>* t)
-{
-	bool resp = true;
-	if (t == NULL)
-	{
-		resp = false;
-	}
-	else
-	{
-		if (el == t->getElemento())
-		{
-			resp = true;
-		}
-		else
-		{
-			resp = buscarR(el, t->getSiguiente());
-		}
+		delete aux; // Borramos memoria
 	}
 	return resp;
 }
 
-template<class T>
-inline bool Lista8<T>::buscar(T el)
-{
-	bool resp = true;
-	Tripla<T>* aux = primer;
-	while (aux != NULL)
-	{
-		if (aux->getElemento() == el)
-		{
-			resp = true;
-			break;
-		}
-		aux = aux->getSiguiente();
-		resp = false;
-	}
-	return resp;
-}
-
-template<class T>
-inline Tripla<T>* Lista8<T>::buscarTripla(T el)
-{
-	Tripla<T>* aux = primer;
-	while (aux != NULL)
-	{
-		if (aux->getElemento() == el)
-		{
-			return aux;
-		}
-		aux = aux->getSiguiente();
-	}
-	return NULL;
-}
 
 template<class T>
 inline void Lista8<T>::mostrar()
@@ -207,25 +280,8 @@ inline void Lista8<T>::mostrar()
 	Tripla<T>* aux = primer;
 	while (aux != NULL)
 	{
-		cout << " " << aux->getElemento() << "(" << aux->getPeso() << ")";
+		cout << " " << aux->getElemento(); // Solo mostramos el elemento
 		aux = aux->getSiguiente();
-	}
-	cout << endl;
-}
-
-template<class T>
-inline void Lista8<T>::mostrarconPesosFinal()
-{
-	Tripla<T>* aux = primer;
-	while (aux != NULL)
-	{
-		cout << " " << aux->getElemento();
-		if (aux->getSiguiente() == NULL)
-		{
-			cout << "(" << aux->getPeso() << ")";
-		}
-		aux = aux->getSiguiente();
-
 	}
 	cout << endl;
 }
@@ -234,51 +290,38 @@ template<class T>
 inline bool Lista8<T>::eliminar_el_dado(T el)
 {
 	bool resp = true;
-	if (primer == NULL) // si no hay ningun elemento en la lista
+	if (primer == NULL)
 	{
 		resp = false;
 	}
 	else
 	{
-		if (primer == ultimo) // si hay un elemento en la lista
+		// Caso especial: El elemento está en el principio
+		if (primer->getElemento() == el)
 		{
-			if (primer->getElemento() == el)
-			{
-				delete primer;
-				primer = NULL;
-				ultimo = NULL;
-			}
-			else
-			{
-				resp = false;
-			}
+			eliminar_Principio();
 		}
-		else // si hay varios elementos en la lista
+		// Caso especial: El elemento está en el final
+		else if (ultimo->getElemento() == el)
 		{
-			Tripla<T>* aux = primer;
-			while (aux != ultimo && aux->getElemento() != el)
+			eliminar_Final();
+		}
+		else
+		{
+			// Buscamos en el medio
+			Tripla<T>* aux = primer->getSiguiente();
+			while (aux != NULL && aux->getElemento() != el)
 			{
 				aux = aux->getSiguiente();
 			}
-			if (aux->getElemento() == el) // si hay una tripla con el elemento
+
+			if (aux != NULL) // Lo encontramos en medio
 			{
-				if (aux == primer)
-				{
-					eliminar_Principio();
-				}
-				else if (aux == ultimo)
-				{
-					eliminar_Final();
-				}
-				else
-				{
-					aux->getAnterior()->setTriplaSig(aux->getSiguiente());
-					aux->getSiguiente()->setTriplaAnt(aux->getAnterior());
-					aux->setTriplaSig(NULL); //ponemos q su sig sea null para no tener problemas con el destructor de la tripla
-					delete aux;
-				}
+				aux->getAnterior()->setTriplaSig(aux->getSiguiente());
+				aux->getSiguiente()->setTriplaAnt(aux->getAnterior());
+				delete aux;
 			}
-			else // si no hay ninguna tripla con el elemento
+			else // No encontrado
 			{
 				resp = false;
 			}
@@ -287,29 +330,92 @@ inline bool Lista8<T>::eliminar_el_dado(T el)
 	return resp;
 }
 
+// --- OPTIMIZACIÓN PARA KARGER ---
+// Esta funcion permite fusionar listas en O(1) en lugar de O(N)
 template<class T>
-inline void Lista8<T>::llenar_Lista8con1M(T n)
+inline void Lista8<T>::absorberLista(Lista8<T>& otraLista)
 {
-	for (int i = 0; i < 10000; i++)
+	// Si la otra lista está vacía, no hay nada que hacer
+	if (otraLista.primer == NULL) return;
+
+	// Si mi lista está vacía, tomo todo de la otra
+	if (this->primer == NULL)
 	{
-		//insertar_Final(n);
+		this->primer = otraLista.primer;
+		this->ultimo = otraLista.ultimo;
 	}
+	else
+	{
+		// Conectamos el final de mi lista con el principio de la otra
+		this->ultimo->setTriplaSig(otraLista.primer);
+		otraLista.primer->setTriplaAnt(this->ultimo);
+
+		// Actualizamos mi nuevo ultimo
+		this->ultimo = otraLista.ultimo;
+	}
+
+	// Vaciamos la otra lista (para que su destructor no borre nada)
+	otraLista.primer = NULL;
+	otraLista.ultimo = NULL;
 }
 
+
+// EN LISTA8.H - Agregar o reemplazar
+
+// Esta función elimina TODAS las ocurrencias de 'el'.
+// Es vital porque al fusionar nodos se crean múltiples bucles.
 template<class T>
-inline Tripla<T>* Lista8<T>::buscarEnesimo(int pos)
+inline void Lista8<T>::eliminar_todos(T el)
 {
-	if (pos < 0)
-	{
-		return NULL;
-	}
+	Tripla<T>* actual = primer;
 
-	Tripla<T>* aux = primer;
-	while (aux != NULL && pos - 1 > 0) //si tomamos como nuestro primer nodo como pos 0 entonces seria pos>0, pero si tomamos q nuestro primer nodo es 1, entonces pos-1>0
+	while (actual != NULL)
 	{
-		aux = aux->getSiguiente();
-		pos--;
-	}
-	return aux; // Puede ser NULL si pos está fuera de rango
-}
+		// Guardamos el siguiente antes de (posiblemente) borrar el actual
+		Tripla<T>* siguiente = actual->getSiguiente();
 
+		if (actual->getElemento() == el)
+		{
+			// --- Lógica de borrado (similar a eliminar_el_dado pero iterativa) ---
+
+			// Caso 1: Es el primero
+			if (actual == primer)
+			{
+				primer = primer->getSiguiente();
+				if (primer != NULL) primer->setTriplaAnt(NULL);
+				else ultimo = NULL; // Quedó vacía
+			}
+			// Caso 2: Es el último
+			else if (actual == ultimo)
+			{
+				ultimo = ultimo->getAnterior();
+				ultimo->setTriplaSig(NULL);
+			}
+			// Caso 3: Está en el medio
+			else
+			{
+				actual->getAnterior()->setTriplaSig(actual->getSiguiente());
+				actual->getSiguiente()->setTriplaAnt(actual->getAnterior());
+			}
+
+			// Importante: Borrar el nodo actual y anular sus punteros para que el destructor no falle
+			actual->setTriplaSig(NULL);
+			// (Nota: Tripla ya tiene destructor vacío, así que delete es seguro)
+			delete actual;
+		}
+
+		// Avanzamos
+		actual = siguiente;
+	}
+}*/
+
+/*
+Por qué absorberLista?
+Cuando el nodo A se coma al nodo B:
+
+En vez de hacer un ciclo while sacando elementos de B e insertándolos en A (lento).
+
+Llamarás a listaDeA.absorberLista(listaDeB).
+
+Esto simplemente cambia dos punteros. ¡Es instantáneo! Esto te ahorrará muchísimo tiempo en las 50,000 iteraciones.
+*/
