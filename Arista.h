6036329da1914fac0e322aa6acd8b9ffa32c8569
ArianaPita-0102 <ariana.pita@ucb.edu.bo>
2025-11-30 16:56:25 -0400

    cambios por referencia en algunas funciones

--- Arista.h
+++ Arista.h
@@ -10,9 +10,33 @@ private:
 	T origen;
 	T destino;
 public:
-	Arista(T o, T d) : origen(o), destino(d) {}
+	Arista() {}
+	Arista(const T& o, const T& d) : origen(o), destino(d) {}
+
+	// Getters
+	T getOrigen() const { return origen; }
+	T getDestino() const { return destino; }
+
+	// Setters 
+	void setOrigen(const T& o) { origen = o; }
+	void setDestino(const T& d) { destino = d; }
+};
+
+
+///version antigua
+/*
+
+template <class T>
+class Arista
+{
+private:
+	T origen;
+	T destino;
+public:
+	//Arista(T o, T d) : origen(o), destino(d) {}
 	T getOrigen() const { return origen; }
 	T getDestino() const { return destino; }
 	void setOrigen(T o) { origen = o; }
 	void setDestino(T d) { destino = d; }
 };
+*/
