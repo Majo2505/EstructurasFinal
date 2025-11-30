b16f439a81224053104bb24b0eb57e8b2f009332
ArianaPita-0102 <ariana.pita@ucb.edu.bo>
2025-11-30 16:55:39 -0400

    Atributo de l8 a adyacencia

--- Vertice.h
+++ Vertice.h
@@ -1,60 +1,55 @@
 #pragma once
 #include "Lista8.h"
+#include <string> 
+using namespace std;
 
 template<class T>
 class Vertice
 {
-
 private:
-	bool marca;
 	string elemento;
-	Lista8<T>* l8;
+	// Representa la lista de vecinos conectados a este vértice.
+	Lista8<T>* adyacencia;
 	string padre;
-	int distancia;
+
 public:
 	Vertice();
 	~Vertice();
+
+	// Devuelve la referencia a la lista de adyacencia
 	Lista8<T>& getLista();
-	bool getMarca();
+
 	string getPadre();
 	string getElementoV();
-	int getDistancia();
-	void setDistancia(int dis);
-	void setElemento(string el);
-	void setMarca(bool m);
-	void setPadre(string p);
 
+	void setElemento(string el);
+	void setPadre(string p);
 };
 
 template<class T>
 inline Vertice<T>::Vertice()
 {
-	padre = -1;
-	marca = false;
-	this->l8 = new Lista8<T>;
-	distancia = 999;
+	padre = ""; // Inicializamos padre como cadena vacía
+
+	// Instanciamos la lista de adyacencia
+	this->adyacencia = new Lista8<T>;
 }
 
 template<class T>
 inline Vertice<T>::~Vertice()
 {
-	if (l8 != nullptr)
+	if (adyacencia != nullptr)
 	{
-		delete l8;
-		l8 = nullptr;
+		delete adyacencia; // Borramos la lista de memoria
+		adyacencia = nullptr;
 	}
 }
 
+// CORRECCIÓN AQUÍ: Agregado <T> después de Lista8
 template<class T>
 inline Lista8<T>& Vertice<T>::getLista()
 {
-	return *l8;
-}
-
-template<class T>
-inline bool Vertice<T>::getMarca()
-{
-	return marca;
+	return *adyacencia;
 }
 
 template<class T>
@@ -70,31 +65,13 @@ inline string Vertice<T>::getElementoV()
 }
 
 template<class T>
-inline int Vertice<T>::getDistancia()
-{
-	return distancia;
-}
-
-template<class T>
-inline void Vertice<T>::setDistancia(int dis)
-{
-	this->distancia = dis;
-}
-
-template<class T>
-inline void Vertice<T>::setMarca(bool m)
-{
-	this->marca = m;
-}
-
-template<class T>
 inline void Vertice<T>::setPadre(string p)
 {
 	this->padre = p;
 }
+
 template<class T>
 inline void Vertice<T>::setElemento(string el)
 {
 	this->elemento = el;
 }
-
